ab6e58e5bfa47a3e82e99ef627a5a12466be74dd
Oleksandr Kukoba <alekskukoba@gmail.com>
2022-10-17 22:20:31 +0300

    Fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CookiesProvider } from 'react-cookie'
 import { appWithTranslation } from 'next-i18next'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
