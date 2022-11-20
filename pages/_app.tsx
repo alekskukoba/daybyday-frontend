f31c8d03cb0595249ec7513e1f29882f62d04305
Oleksandr Kukoba <alekskukoba@gmail.com>
2022-11-20 20:52:45 +0200

    Rename Layout.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CookiesProvider } from 'react-cookie'
 import { appWithTranslation } from 'next-i18next'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
