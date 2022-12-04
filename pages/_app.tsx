d4f28e09fd3fcf3f2aee60e465f6cc0f609a69c9
Oleksandr Kukoba <alekskukoba@gmail.com>
2022-12-04 10:12:46 +0200

    Revert "Add google analythics"
    This reverts commit bade6bbc6cdf98d6e50a4e0c2c24df98da9fc3ad.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import '../styles/globals.css'
 import 'nprogress/nprogress.css' //styles of nprogress
 import 'yet-another-react-lightbox/styles.css'
-import * as ga from '../lib/ga'
-import Router, { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import { CookiesProvider } from 'react-cookie'
 import Layout from '../components/Layout'
 import NProgress from 'nprogress' //nprogress module
+import Router from 'next/router'
 import { Toaster } from 'react-hot-toast'
 import { appWithTranslation } from 'next-i18next'
-import { useEffect } from 'react'
 
 NProgress.configure({ showSpinner: false })
 
@@ -19,18 +17,6 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      ga.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return (
     <CookiesProvider>
       <Layout>
